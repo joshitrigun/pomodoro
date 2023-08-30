f46d3eab9da8c2efd8a0bef2fb27aed67db3cec5
Trigun Joshi <trigunjoshi@gmail.com>
2023-08-29 23:59:53 -0700

    setting timer session and values

--- main.py
+++ main.py
@@ -10,13 +10,28 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        countdown(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -28,6 +43,9 @@ def countdown(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
+
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
