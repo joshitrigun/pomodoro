9442549f752d484899af2fe1d57625919d53e115
Trigun Joshi <trigunjoshi@gmail.com>
2023-08-29 23:34:58 -0700

    timer functionality added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,11 +10,20 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    countdown(5*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,22 +35,23 @@ window.config(padx=100, pady=200, bg=YELLOW)
 title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, 'bold'))
 title_label.grid(column=1, row=0)
 
-
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 102, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 102, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
+
+
 
 def action():
     pass
 
 
 # calls action when pressed
-button = Button(text="Start", command=action, highlightthickness=0)
+button = Button(text="Start", command=start_timer, highlightthickness=0)
 button.grid(column=0, row=2)
 
-button1 = Button(text="Reset", command=action, highlightthickness=0)
+button1 = Button(text="Reset", command=start_timer, highlightthickness=0)
 button1.grid(column=2, row=2)
 
 check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
