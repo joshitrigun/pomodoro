57ca82d73b86621b82524b7d5d97ebfa9c5f32c4
Trigun Joshi <trigunjoshi@gmail.com>
2023-08-29 23:44:57 -0700

    dynamic typing added in timer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,11 +16,15 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(5*60)
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    countdown(5 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
-    count_min = math.floor(count/60)
+    count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
@@ -40,7 +45,6 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 102, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
-
 
 
 def action():
